0dce7e1ed6081d826341df925356153cb4fba69c
nicchongwb <nicholaschongwb@gmail.com>
2025-01-28 01:25:55 +0800

    update

---
+++ simulate_admin.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+import requests
+
+url = 'http://172.17.0.2:80' # local
+
+proxies = {
+	'http': '127.0.0.1:8080',
+	'https': '127.0.0.1:8080'
+}
+
+def main():
+    # for simulating admin session
+	s = requests.Session()
+	data = {'username':'admin','password':'admin'}
+
+	s.get(f"{url}/login.php")
+	s.post(f"{url}/login.php", data=data, proxies=proxies)
+
+if __name__ == "__main__":
+	main()
